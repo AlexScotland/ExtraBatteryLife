4f9f688221af364b6e2fc1a3ef9dfb99cc2e3461
Alex Scotland <alex@scotland.ca>
2024-04-13 19:38:50 -0400

    feat(battery): Adds battery life to 120 seconds, not 90.

---
+++ ExtraBatteryLife/Patches/VideoCameraTimeLeftPatch.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraBatteryLife.Patches
+{
+    [HarmonyPatch(typeof(VideoCamera))]
+    internal class VideoCameraTimeLeftPatch
+    {
+        // Patching Method
+        [HarmonyPatch(typeof(VideoCamera), "ConfigItem")]
+        [HarmonyPostfix]
+        static void BatteryLevelPatch(ref VideoInfoEntry ___m_recorderInfoEntry)
+        {
+            float originalMaxTime = 90f; // This is the original value the game has - 90 seconds
+            float newMaxTime = 120f; // Let's make it 2 minutes
+            if (___m_recorderInfoEntry.maxTime == originalMaxTime && ___m_recorderInfoEntry.timeLeft == originalMaxTime)
+            {
+                // This is true when we make a new Camera Object
+                ___m_recorderInfoEntry.maxTime = newMaxTime;
+                ___m_recorderInfoEntry.timeLeft = newMaxTime;
+            }
+        }
+    }
+}
--- ExtraBatteryLife/Plugin.cs
+++ ExtraBatteryLife/Plugin.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BepInEx;
+using BepInEx.Logging;
+using ExtraBatteryLife.Patches;
+using HarmonyLib;
 
 namespace ExtraBatteryLife
 {
-    public class ExtraBatteryLifeBase
+    [BepInPlugin(
+        modGUID,
+        modName,
+        modVersion
+        )]
+    public class ExtraBatteryLifeBase : BaseUnityPlugin
     {
+        private const string modGUID = "Dug.ExtraBatteryLife";
+        private const string modName = "Extra Battery Life";
+        private const string modVersion = "1.0.0";
+
+        internal ManualLogSource mls;
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static ExtraBatteryLifeBase Instance;
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            // Loading Mod message...
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo($"Loading {modName} - {modVersion}");
+
+            harmony.PatchAll(typeof(ExtraBatteryLifeBase));
+            harmony.PatchAll(typeof(VideoCameraTimeLeftPatch));
+            mls.LogInfo($"{modName} - {modVersion} is loaded!");
+        }
     }
 }
